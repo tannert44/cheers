738250c7c49e7cb14c3df69b5bf626a4d885ca58
tannert44 <tannert8155@gmail.com>
2015-10-13 15:22:38 -0500

    Figured out the cheer thingy with halfnorsemix dog.

---
+++ Cheers/Cheers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheers
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey What's Your Name?");
+            string name = Console.ReadLine();
+            for(int i=0; i<name.Length; i++)
+            {
+                bool isVowel = "halfnorsemix".IndexOf(name[i].ToString(), StringComparison.InvariantCultureIgnoreCase) >= 0;
+                if(isVowel)
+                {
+                    Console.WriteLine("Give me an ... " + name[i]);
+                }
+                else
+                {
+                    Console.WriteLine("Give me a ..." + name[i]);
+                }
+                 
+            }
+            Console.WriteLine("Goooooo " + name + "!");
+
+
+        }
+    }
+}
